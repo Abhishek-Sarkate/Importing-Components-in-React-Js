b37d20d0d1e6b888c1c9fb7fc84e9d2c0c1056ac
Abhishek-Sarkate <a.sarkate301198@gmail.com>
2021-02-13 00:46:16 +0530

    newcomponents

---
+++ src/App.js
@@ -0,0 +1,29 @@
+
+import './Abhi.css'
+function App()
+{
+  return (
+    <div>
+              <h1>
+                Here we have used...........BY TAG
+                </h1>
+
+                                        <h2 className='class'>
+
+
+                                        Here we have used...........BY CLASS
+
+                                        </h2>
+  
+                                                                <p id='Ptag'>
+
+                                                                Here we have used..........BY ID
+
+                                                                </p>
+
+     </div>
+    
+  );
+}
+
+export default App;
